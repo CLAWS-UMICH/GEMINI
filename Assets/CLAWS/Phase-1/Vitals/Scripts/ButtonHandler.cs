8dd33a0f060afac67688ae27652ad420e84e4a4a
yatinbichala <bichalay@umich.edu>
2026-05-05 21:40:08 -0400

    added buttonsin UI to radial vitals

---
+++ Assets/CLAWS/Phase-1/Vitals/Scripts/ButtonHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void ButtonRightClick()
+    {
+        
+    }
+    
+    public void ButtonLeftClick()
+    {
+        
+    }
+}
